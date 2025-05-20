f69fceb035ccb5a822b41b592128174d5bb6e9c0
BizRaf <chigzah@gmail.com>
2025-05-20 21:12:58 +0300

    Update #libClient.h

--- Code/#libClient.h
+++ Code/#libClient.h
@@ -1,8 +1,25 @@
 #pragma once
 
 #include "#libEntity.h"
-#include "#libEntityVec.h"
+//#include "#libEntityVec.h"
 #include "#libWallet.h"
+#include <string>
+#include <vector>
 
 using namespace std;
 
+class Client : public Entity {
+protected:
+	string name;
+	//EntityVec wallets;
+	vector<Wallet*> vWallets;
+
+public:
+	Client(string id, string name);
+	void addWallet(Wallet* wallet);
+	double getTotalBalance();
+	virtual double calculateCommission(double amount) = 0;
+	virtual double getMaxTransactionLimit() = 0;
+	string get_id() { return id; };
+	virtual ~Client() = 0;
+};
