3aeb4ba96cd63b7d6ce5a128a7d6821687415f97
BizRaf <chigzah@gmail.com>
2025-05-20 21:13:29 +0300

    Update #libTypesClient.h

--- Code/#libTypesClient.h
+++ Code/#libTypesClient.h
@@ -3,3 +3,11 @@
 #include "#libClient.h"
 
 using namespace std;
+
+class GoldClient : public Client {
+public:
+	GoldClient(string id, string name);
+	double calculateCommission(double amount);
+	double getMaxTransaction();
+	string getBenefits();
+}
