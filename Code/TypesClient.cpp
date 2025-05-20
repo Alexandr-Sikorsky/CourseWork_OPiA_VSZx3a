82f0ddb08452cab97390bd47828b77b6147a9a18
BizRaf <chigzah@gmail.com>
2025-05-20 21:14:05 +0300

    Update TypesClient.cpp

--- Code/TypesClient.cpp
+++ Code/TypesClient.cpp
@@ -2,5 +2,15 @@
 
 #include "#libTypesClient.h"
 
-using namespace std;
-
+GoldClient::GoldClient(string id, string name):Client(id, name){
+	std::cout << "0K\n";
+}
+double GoldClient::calculateCommission(double amount) {
+	return 1.0;
+}
+double GoldClient::getMaxTransaction() {
+	return 1000;
+}
+string GoldClient::getBenefits() {
+	return "Prior processing, low commission";
+}
