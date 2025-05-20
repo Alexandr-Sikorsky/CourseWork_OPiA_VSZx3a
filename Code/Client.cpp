17a4444088f7050636208a4834aff7098474aea6
BizRaf <chigzah@gmail.com>
2025-05-20 21:12:23 +0300

    Update Client.cpp

--- Code/Client.cpp
+++ Code/Client.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 #include "#libClient.h"
+#include <string>
 
 using namespace std;
 
+Client::Client(string id, string name):
+	Entity(id) {
+	this->name = name;
+}
+void Client::addWallet(Wallet* wallet) {
+	vWallets.push_back(wallet);
+}
+double Client::getTotalBalance() {
+	double total_balance = 0.0;
+	for (int i = 0; i < vWallets.size(); i++) {
+		total_balance += vWallets.at(i)->get_balance();
+	}
+	return total_balance;
+}
