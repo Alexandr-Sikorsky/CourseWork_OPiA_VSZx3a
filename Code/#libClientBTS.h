e04da5dd07d869d0e8f4ace57e9e048dca60cf07
BizRaf <chigzah@gmail.com>
2025-06-05 13:11:58 +0300

    Update #libClientBTS.h

--- Code/#libClientBTS.h
+++ Code/#libClientBTS.h
@@ -2,3 +2,21 @@
 
 #include "#libClientNode.h"
 
+class ClientBST {
+public:
+	ClientNode* root;
+
+private:
+	void insertNode(ClientNode *&current, Client* client);
+	ClientNode* removeNode(ClientNode*& current, string id);
+	ClientNode* findNode(ClientNode *current, string id);
+	void showTree(const ClientNode* current);
+
+public:
+	ClientBST() { root = nullptr; };
+	void insert(Client* client) { insertNode(root, client); };
+	bool remove(string id) { removeNode(root, id); return false; };
+	Client* find(string id) { return findNode(root, id)->data; };
+	void displayInOrder() { return showTree(root); };
+	~ClientBST() {};
+};
