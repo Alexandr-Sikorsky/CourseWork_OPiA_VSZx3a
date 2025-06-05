479617f4da84ccb577eb13062b9c09e4fd98655c
BizRaf <chigzah@gmail.com>
2025-06-05 13:14:07 +0300

    Update ClientBTS.cpp

--- Code/ClientBTS.cpp
+++ Code/ClientBTS.cpp
@@ -4,3 +4,91 @@
 
 using namespace std;
 
+
+void ClientBST::insertNode(ClientNode*& current, Client* client) {
+	if (current == nullptr) {
+		ClientNode* newNode = new ClientNode(client);
+		current = newNode;
+	}
+	else if (client->get_id() < current->data->get_id()) {
+		insertNode(current->left, client);
+	}
+	else if (client->get_id() > current->data->get_id()) {
+		insertNode(current->right, client);
+	}
+	else {
+		cout << "Client is already in the tree" << endl;
+	}
+}
+ClientNode* ClientBST::removeNode(ClientNode*& current, string id) {
+	if (current == nullptr) {
+		return nullptr;
+	}
+
+	if (current->data->get_id() == id) {
+		if (current->left == nullptr && current->right == nullptr) {
+			delete current;
+			return nullptr;
+		}
+		else if (current->left != nullptr && current->right == nullptr) {
+			ClientNode* orphan = current->left;
+			delete current;
+			return orphan;
+		}
+		else if (current->right != nullptr && current->left == nullptr) {
+			ClientNode* orphan = current->right;
+			delete current;
+			return orphan;
+		}
+		else {
+			if (current->right->left == nullptr) {
+				current->data = current->right->data;
+				current->right = current->right->right;
+			}
+			else {
+				ClientNode* lefmostNode = current->right;
+				while (lefmostNode->left != nullptr) {
+					lefmostNode = lefmostNode->left;
+				}
+				current->data = lefmostNode->data;
+				removeNode(current->right, lefmostNode->data->get_id());
+			}
+			return current;
+		}
+	}
+	else if (id < current->data->get_id() && current->left != nullptr) {
+		current->left = removeNode(current->left, id);
+		return current;
+	}
+	else if (id > current->data->get_id() && current->right != nullptr) {
+		current->right = removeNode(current->right, id);
+		return current;
+	}
+	else {
+		cout << "No such node!" << endl;
+		return current;
+	}
+	return current;
+}
+ClientNode* ClientBST::findNode(ClientNode* current, string id) {
+	if (current == nullptr) {
+		return nullptr;
+	}
+	else if (current->data->get_id() == id) {
+		return current;
+	}
+	else if (current->data->get_id() < id) {
+		return findNode(current->left, id);
+	}
+	else {
+		return findNode(current->right, id);
+	}
+}
+void ClientBST::showTree(const ClientNode* current) {
+	if (current == nullptr)
+		return;
+
+	showTree(current->left);
+	cout << current->data->get_id() << '\t' << current->data->name << '\n';
+	showTree(current->right);
+}
