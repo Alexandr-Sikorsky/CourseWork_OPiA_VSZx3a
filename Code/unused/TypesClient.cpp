94b3ff336fe37e4671794c141d6fdc5851939e15
Alexandr-Sikorsky <megasasha200524@gmail.com>
2025-06-05 13:23:49 +0300

    Add files via upload

---
+++ Code/unused/TypesClient.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "#libTypesClient.h"
+
+//StandardClient::StandardClient(string id, string name) :Client(id, name) { }
+//double StandardClient::calculateCommission(double amount) {
+//	return amount * 0.05;
+//}
+//double StandardClient::getMaxTransactionLimit() {
+//	return 1000;
+//}
+//string StandardClient::getBenefits() {
+//	return "Standard access";
+//}
+//
+//GoldClient::GoldClient(string id, string name) :Client(id, name) { }
+//double GoldClient::calculateCommission(double amount) {
+//	return amount * 0.01;
+//}
+//double GoldClient::getMaxTransactionLimit() {
+//	return 10000;
+//}
+//string GoldClient::getBenefits() {
+//	return "Prior processing, low commission";
+//}
