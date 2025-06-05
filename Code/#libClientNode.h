9a215497039e4a4e673ee3a98ec99b3f30db3d83
BizRaf <chigzah@gmail.com>
2025-06-05 13:11:44 +0300

    Update #libClientNode.h

--- Code/#libClientNode.h
+++ Code/#libClientNode.h
@@ -1,4 +1,14 @@
 #pragma once
+#include "#libClient.h"
 
 using namespace std;
 
+class ClientNode {
+public:
+	Client* data;
+	ClientNode* left;
+	ClientNode* right;
+
+	ClientNode(Client* data) { this->data = data; left = nullptr; right = nullptr; };
+	~ClientNode() { left = nullptr; right = nullptr; };
+};
