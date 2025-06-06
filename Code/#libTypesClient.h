85f71ddb58ffadf968470f2e161e2a7ca2ffc982
BizRaf <chigzah@gmail.com>
2025-06-06 12:18:56 +0300

    Update #libTypesClient.h

--- Code/#libTypesClient.h
+++ Code/#libTypesClient.h
@@ -4,10 +4,25 @@
 
 using namespace std;
 
+
 class GoldClient : public Client {
 public:
-	GoldClient(string id, string name);
-	double calculateCommission(double amount);
-	double getMaxTransaction();
-	string getBenefits();
+	GoldClient(string id, string name) : Client(id, name) { this->type = CLIENTTYPE_GOLD; };
+	virtual double calculateCommission(double amount) { return amount * 0.01; };
+	virtual double getMaxTransactionLimit() { return 10000; };
+	virtual string getBenefits() { return "Priority processing, low fees"; };
+};
+class PlatinumClient : public Client {
+public:
+	PlatinumClient(string id, string name) : Client(id, name) { this->type = CLIENTTYPE_PLATINUM; };
+	virtual double calculateCommission(double amount) { return amount * 0.02; };
+	virtual double getMaxTransactionLimit() { return 5000; };
+	virtual string getBenefits() { return "Bonus rewards, moderate fees"; };
+};
+class StandardClient: public Client {
+public:
+	StandardClient(string id, string name) : Client(id, name) { this->type = CLIENTTYPE_STANDARD; };
+	virtual double calculateCommission(double amount) { return amount * 0.05; };
+	virtual double getMaxTransactionLimit() { return 1000; };
+	virtual string getBenefits() { return "Standard access"; };
 };
