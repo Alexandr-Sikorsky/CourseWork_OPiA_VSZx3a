79617fba29845c5f422ecba6e4fa1021b4d1a999
BizRaf <chigzah@gmail.com>
2025-06-06 16:13:19 +0300

    Update #libBlockchain.h

--- Code/#libBlockchain.h
+++ Code/#libBlockchain.h
@@ -1,14 +1,20 @@
 #pragma once
 
+#include <string>
+#include "#libTypesClient.h"
 #include "#libClientBST.h"
 #include "#libTransactList.h"
 
 class Blockchain { 
 private:
+	string savefile;
 	ClientBST clients;
 	TransactionList transactions;
+
+	void writeClientNode(ClientNode* currentNode, string path);
+
 public:
-	Blockchain();
+	Blockchain(string path = "t.txt");
 	void addClient(Client* client);
 	bool processTransaction(Transaction* tx);
 	void displayClients();
