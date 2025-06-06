488604eb0ae9cb62d70063defa8d5371074480f5
BizRaf <chigzah@gmail.com>
2025-06-06 13:19:54 +0300

    Update Wallet.cpp

--- Code/Wallet.cpp
+++ Code/Wallet.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 #include "#libWallet.h"
+
 #include <string>
 
 using namespace std;
 
-Wallet::Wallet(string id, double balance, Client& owner) :
+Wallet::Wallet(string id, double balance, string owner_id) :
     Entity(id) {
     this->balance = balance;
-    this->owner_id = owner.get_id();
-    owner.addWallet(this);
+    this->owner_id = owner_id;
 }
 
 // Wallet::Wallet (const Wallet &wal):
@@ -18,28 +18,28 @@ Wallet::Wallet(string id, double balance, Client& owner) :
 //         this->owner_id = wal.owner_id;
 // }
 
-void Wallet::deposit (double amount){
+void Wallet::deposit(double amount) {
     if (amount > 0) {
         this->balance += amount;
     }
-    else{
-        cout<<"Incorrect amount of deposit"<<endl;
+    else {
+        cout << "Incorrect amount of deposit" << endl;
     }
 }
 
-bool Wallet::withdraw (double amount){
+bool Wallet::withdraw(double amount) {
     if (amount > 0 && this->balance >= amount) {
         this->balance -= amount;
         return true; // uspeshnoe snyatie
     }
-    cout<<"Error withdraw"<<endl;
+    cout << "Error withdraw" << endl;
     return false; // nedostatochno sredstv ili nevernya summa
 }
 
-double Wallet::get_balance(){
+double Wallet::get_balance() {
     return balance; //this->---?
 }
 
-string Wallet::get_id(){
+string Wallet::get_id() {
     return id;
 }
