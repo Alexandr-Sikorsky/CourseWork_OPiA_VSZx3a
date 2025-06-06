dfc39eca992e916f3c9bb87c89f6b591a417f6c1
BizRaf <chigzah@gmail.com>
2025-06-06 13:19:37 +0300

    Update #libWallet.h

--- Code/#libWallet.h
+++ Code/#libWallet.h
@@ -1,24 +1,25 @@
 #pragma once
 
 #include "#libEntity.h"
-#include "#libClient.h"
+
 
 using namespace std;
 
-class Wallet: public Entity{
+class Wallet : public Entity {
 
-    private:
-        //string id; !!!!!!!Oshibka kotoruyu pytalsya nayti minut 40 - 
-        //ne pishite v konstruktore peremennyye, kotoryye nasleduyutsya
-        double balance;
-        string owner_id;
-        friend class Transaction;
-        friend class Blockchain;
-    public:
-        Wallet(string id, double balance, Client& owner);
-        //Wallet(const Wallet &wal);
-        void deposit (double amount);
-        bool withdraw (double amount);
-        double get_balance();
-        string get_id();
+private:
+    //string id; !!!!!!!Oshibka kotoruyu pytalsya nayti minut 40 - 
+    //ne pishite v konstruktore peremennyye, kotoryye nasleduyutsya
+    double balance;
+    string owner_id;
+    friend class Transaction;
+    friend class Blockchain;
+public:
+    Wallet(string id, double balance, string owner_id);
+    //Wallet(const Wallet &wal);
+    void deposit(double amount);
+    bool withdraw(double amount);
+    double get_balance();
+    string get_id();
+    void set_owner_id(string id) { owner_id = id; };
 };
