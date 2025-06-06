9d7d834f5a304d06f5cf0791975713e091be0ccd
BizRaf <chigzah@gmail.com>
2025-06-06 18:01:25 +0300

    Update #libTransact.h

--- Code/#libTransact.h
+++ Code/#libTransact.h
@@ -4,23 +4,39 @@
 
 using namespace std;
 
-enum class TxType{
+enum class TxType {
     TRANSFER,    // Perevod
     DEPOSIT,     // Vnesenie nal
     WITHDRAWAL  // Snyatie nal
 };
 
-class Transaction : public Entity{
-    private:
-        string senderWalletId;
-        string recipientWalletId;
-        double amount;
-        TxType type;
-        double commission;
-        friend class Blockchain;
-        friend class TransactionNode;
-    public:
-        Transaction(string id, string senderWalletId, string recipientWalletId, double amount, TxType type, double commission);
-        string get_id();
-        string getDetails();
+class Transaction : public Entity {
+private:
+    string senderWalletId;
+    string recipientWalletId;
+    double amount;
+    TxType type;
+    double commission;
+    friend class Blockchain;
+    friend class TransactionNode;
+public:
+    Transaction(string id, string senderWalletId, string recipientWalletId, double amount, TxType type, double commission);
+    string get_id();
+    string getDetails();
+    char get_type() {
+        switch (type)
+        {
+        case TxType::TRANSFER:
+            return 't';
+            break;
+        case TxType::DEPOSIT:
+            return 'd';
+            break;
+        case TxType::WITHDRAWAL:
+            return 'w';
+            break;
+        default:
+            break;
+        }
+    };
 };
